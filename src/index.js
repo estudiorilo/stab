18cc058d0ac094f2c0fb67e379e40fcd9c7cbdd6
caiokawasaki <caiokawasaki@gmail.com>
2019-05-23 17:03:22 -0300

    export default is now available

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-export * from './components';
+import * as components from './components'
+
+const install = (Vue) => {
+    Object.keys(components).forEach((name) => {
+        Vue.component(name, components[name])
+    })
+}
+
+export * from './components'
+export default install
