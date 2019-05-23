e2fd9b3d84d8b5a4684d7744311b9d0e3c7b4624
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2019-05-22 23:42:06 -0300

    Remove main and only exports components on index

--- src/components/index.js
+++ src/components/index.js
@@ -1,14 +1,2 @@
-import stabContainer from './StabContainer';
-
-export const StabContainer = stabContainer;
-
-const components = {
-    StabContainer
-};
-
-export default {
-    install(Vue) {
-        Object.keys(components)
-            .forEach(name => Vue.component(name, components[name]))
-    }
-}
+export { default as StabContainer } from './StabContainer.vue';
+export { default as StabLink } from './StabLink.vue';
